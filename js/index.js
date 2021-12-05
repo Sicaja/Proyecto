57d1228437340289bd19477d35526912a02a9135
Julio Sicajá <multi.julio.sicaja@gmail.com>
2021-12-05 05:20:36 -0600

    agregar modal

--- js/index.js
+++ js/index.js
@@ -2,9 +2,15 @@
 const burgerIcon = document.getElementById("burger");
 const navbarMenu = document.getElementById("nav-links");
 
+const detailModal = document.getElementById("modalDetail");
+
 //Add event listener
 
 burgerIcon.addEventListener("click", ()=>{
     navbarMenu.classList.toggle('is-active');
     burgerIcon.classList.toggle('is-active');
 });
+
+function openDetail() {
+    detailModal.classList.toggle('is-active');
+}
